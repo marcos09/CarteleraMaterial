0cf508c85a7d9d1928455b8e7a189e981d0a4c60
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-07-21 03:10:02 -0300

    Cambios interfaz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {MdDialog, MdDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+	  constructor(public dialog: MdDialog) {}
+
   title = 'app works!';
+  carteleras = ["CADP","AyED","TTPS Java", "Organización de computadoras"];
+  color = 'accent';
+  checked = false;
+  disabled = false;
+
+  openDialog() {
+    this.dialog.open(DialogOverviewExampleDialog);
+  }
 }
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog.html',
+})
+export class DialogOverviewExampleDialog {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule, MdMenuModule} from '@angular/material';
+import {MaterialModule} from'@angular/material';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -17,13 +16,33 @@ import 'hammerjs';
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule,
-    MdButtonModule, 
-    MdCheckboxModule,
-    MdMenuModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
+/*
+import {MdCardModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdMenuModule} from '@angular/material';
+import {MdSidenavModule} from '@angular/material';
+import {MdAutocompleteModule} from '@angular/material';
+import {MdInputModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+
+    BrowserAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdAutocompleteModule,
+    MdInputModule,
+    MdIconModule,
+    MdToolbarModule
+
+
+*/
